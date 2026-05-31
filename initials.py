2ea0d5fb4ae34c0b4615f342a34bac7fe9ca469e
Joseph_Bio <josephbiossey03@gmail.com>
2026-05-31 22:55:47 +0000

    Complete final assignment requirement

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Prints out my initials in large letters.
+# Assignment Number: 1
+#
+# Name: Joseph Apelete Biossey
+# STUDENT ID: 2425401486
+# Email: 2425401486@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+#
+# On my honor,Joseph Apelete Biossey, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Function to print out my initials in large letters
 
+    print()
+    print("...JAB")
+    print()
+
+    print("...JJJJJJJJJJJJ...  ........AA........  ...BBBBBBBBBB.....  ")
+    print("............JJ....  .......AAAA.......  ...BB.......BB....  ")
+    print("............JJ....  ......AA..AA......  ...BB........BB...  ")
+    print("............JJ....  .....AA....AA.....  ...BB.......BB....  ")
+    print("............JJ....  ....AA......AA....  ...BBBBBBBBBB.....  ")
+    print("......JJ....JJ....  ...AAAAAAAAAAAA...  ...BB.......BB....  ")
+    print("......JJ....JJ....  ...AA........AA...  ...BB........BB...  ")
+    print("......JJ....JJ....  ...AA........AA...  ...BB.......BB....  ")
+    print(".......JJJJJJ.....**...AA........AA...**...BBBBBBBBBB.....**")
+    print(".........JJ.......**...AA........AA...**...BBBBBBBBB......**")
+
+    print()
 
 
 main()
